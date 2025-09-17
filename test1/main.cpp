93ea22fb4ab78f88cf7db6adfa281b0093968197
MMSi <namekey90@gmail.com>
2025-09-17 23:57:23 +0200

    12345

---
+++ test1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+	/*
+	Some
+	some
+	some
+	*/
+	
+	setlocale(LC_ALL, "RU");
+	
+	std::cout << "All is okay!\n" << std::endl;
+	std::cout << "Привет, как дела?";
+	
+	return 0;
+}
