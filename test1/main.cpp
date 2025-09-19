333cfd583559339fb114f45a2ac157f89935bb71
MMSi <namekey90@gmail.com>
2025-09-19 19:08:54 +0200

    -gitingore

--- test1/main.cpp
+++ test1/main.cpp
@@ -7,9 +7,10 @@ int main() {
 	some
 	*/
 	
-	setlocale(LC_ALL, "RUSSIA");
+	system ("chcp 1251 > null");
 	
-	std::cout << "All is okay!\n" << std::endl;
+	std::cout << "All is okay!\n" << std::endl; /* \n пропускает строку
+	*/
 	std::cout << "Привет, как дела?";
 	
 	return 0;
