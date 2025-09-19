74cc22a46879e3948f681f24a83887fe46ccef36
MMSi <namekey90@gmail.com>
2025-09-19 20:58:01 +0200

    new text in test1

--- test1/main.cpp
+++ test1/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <iostream> //#include <cmath> математический модуль, если вдруг в иострим нет вдруг их
 
 int main() {
 	/*
@@ -11,6 +11,20 @@ int main() {
 	
 	std::cout << "All is okay!\n" << std::endl; /* \n пропускает строку
 	*/
+	std::cout << "Hello world!!\n" <<std::endl;
+
+	int x;
+	int y;
+	std::cin >> x >>y;
+	std::cout << "x = " << x << "\n" << "y = " << y << std::endl << "x + y = " << x+y;
 	
+	// Арифметические операции
+	//+, -, *, /, %(ост от деления), ++, -
+	// float f = 10.(float) / 3(int)
+	// int x = 10;
+	// int y = 3;
+	// float f = (x*1.f)(вещественным тогда будет) / y
+	// i+=1(как в питоне), i++ постфикный инкремент, i-- декремент, ++i префиксный инкремент 
+	// int q = 3.345f неявное приведение тип,, int w = (int)3.345f явное приведение типа, c-style cast, int e = static_cast<int(целевой тип)>(3.345f)-(приводит один тип к другому) явное приведение типа c++ style(шаблон)
 	return 0;
 }
