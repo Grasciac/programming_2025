62caae5b7b7ff237c6e17f97abd06ca033ea750d
MMSi <namekey90@gmail.com>
2025-10-02 11:06:11 +0200

    add all lab2

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,23 +1,32 @@
 #include <iostream>
 #include <cmath>
+#include <bitset>
 int main()
 {
   setlocale(LC_ALL, "RU");
-  unsigned short i;
-  signed char A;
-  std::cin >> i;
-  A = char(i);
-  std::cin >> i;
-  if (!((signed char)pow(2, i - 1) & A))
+  unsigned short i; 
+  short A;
+  std::cout << "Номер бита = "; std::cin >> i;
+  std::cout << "Число =  "; std::cin >> A;
+  std:: bitset<8> b2 = A;
+  std:: cout << "A in binary = " << b2.to_string() << std:: endl;
+  i = 1 << i;
+  b2 = i;
+  std:: cout << "i bit in binary = " <<b2.to_string() << std::endl;
+  /*
+  A = (A = !(i & A)? (A * A) & ~i : A = 125 & 0b10101010);  //, вместо всей конструкции if else
+  */
+  if (!(i & A)) // не  (i и A) 
   {
-    std::cout << (int) A << std::endl;
-    return 1;
+    std:: cout << A *  A << std::endl;
+    A *= A;
+    A = A & ~i; //заменяем i бит на ноль2
   }
   else
   {
-    
-
+    A = 125 & 0b10101010;
   }
+  b2 = A;
+  std::cout << "Ответ:" << A << std::endl; std:: cout << "Ответ in binary = " << b2.to_string() << std::endl;
   return 0;
 }
-
